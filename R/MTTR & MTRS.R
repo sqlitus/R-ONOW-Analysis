b48fd1149ddd456b09d753bf5a30bb0c385c2bc9
Chris <jcjabr@gmail.com>
2018-08-01 15:42:52 -0500

    finished L1-L3 response times per ticket

--- R/MTTR & MTRS.R
+++ R/MTTR & MTRS.R
@@ -1,32 +1,101 @@
 # Mean Time to Response (MTTR) & Mean Time Restore Service (MTRS) Analysis
-# Combining team pass history & assignment pass history
+# Relies on both team pass history & assignment pass history
 
 library(tidyverse); library(lubridate); library(tools)
 start_time <- Sys.time()
 print(paste("Starting:", start_time))
 
-# import all appropriately named files
+# find all historical files for import ----
 path <- "\\\\cewp1650\\Chris Jabr Reports\\ONOW Exports\\INC History"
 team_history_files <- list.files(path, "(?i)team_hist", full.names = TRUE)
 assign_history_files <- list.files(path, "(?i)assign_hist", full.names = TRUE)
-
-
-team_history <- list.files(path = path, pattern = "(?i)team_hist.*", full.names = TRUE) %>% map_df(readxl::read_excel)
-team_history2 <- team_history_files %>% map_df(readxl::read_excel)
 
 import_files <- function(files){
   out <- data_frame()
   for (i in 1:length(files)){
     if (file_ext(files[i]) == "xlsx") { data <- readxl::read_excel(files[i])
-    } else if (file_ext(files[i]) == "csv") { data <- read.csv(files[i])
+    } else if (file_ext(files[i]) == "csv") { data <- read.csv(files[i])  # make change: convert columns on import
     } else { break }
     data$import_sheet <- str_extract(files[i], "(?<=/).*") # positive lookbehind
-    out <- bind_rows(team_history, data)
+    out <- bind_rows(out, data)
   }
   return(out)
 }
 
-test <- import_files(team_history_files)
+# import team history data & filter out flash assignments ----
+team_history <- import_files(team_history_files) %>% distinct(Number, Field, Value, Start, End, Resolved, State) %>% arrange(Start)
+filter_out_teams <- team_history %>% filter(Start == End)  # anti join to filter out flash assignments, while keeping NA times
+team_history <- team_history %>% anti_join(filter_out_teams)
+
+# import assign history data (from csv) & filter out flash assignments ----
+assign_history <- import_files(assign_history_files) %>% 
+  rename(Number=inc_number, Field=mi_field, Value=mi_value, Start=mi_start, End=mi_end, Resolved=inc_resolved_at, State=inc_state) %>%
+  distinct(Number, Field, Value, Start, End, Resolved, State) %>%
+  arrange(Start)
+assign_history$Number <- as.character(assign_history$Number)
+assign_history$Field <- as.character(assign_history$Field)
+assign_history$Value <- as.character(assign_history$Value)
+assign_history$Start <- as.POSIXct(assign_history$Start, format = "%m-%d-%Y %H:%M:%S", tz = "UTC") # UTC is default tz for other funcs
+assign_history$End <- as.POSIXct(assign_history$End, format = "%m-%d-%Y %H:%M:%S", tz = "UTC")
+assign_history$Resolved <- as.POSIXct(assign_history$Resolved, format = "%m-%d-%Y %H:%M:%S", tz = "UTC")
+assign_history$State <- as.character(assign_history$State)
+filter_out_assigns <- assign_history %>% filter(Start == End)  # anti join to filter out flash assignments, while keeping NA times
+assign_history <- assign_history %>% anti_join(filter_out_assigns)
+
+# compile entire team & analyst passing history ----
+all_history <- bind_rows(team_history, assign_history) %>% arrange(Start)
+all_history <- all_history %>% arrange(Number, Start) %>% group_by(Number) %>% 
+  mutate(event_num = row_number(),
+         prev_time = lag(Start, order_by = Start),
+         prev_action = lag(Field, order_by = Start),
+         response_time = case_when(Field == 'assigned_to' & prev_action == 'assignment_group' ~ 
+                                     difftime(Start, prev_time, units='mins')))
+
+# get first L1/L2/L3 team assignment per ticket ----
+first_L1 <- team_history %>% group_by(Number) %>% filter(Value == 'Retail Support') %>% filter(Start == min(Start)) %>%
+  mutate(first_L1 = 1) %>% select(Number, Start, first_L1)
+first_L2 <- team_history %>% group_by(Number) %>% filter(Value == 'Retail Support L2') %>% filter(Start == min(Start)) %>%
+  mutate(first_L2 = 1) %>% select(Number, Start, first_L2)
+first_L3 <- team_history %>% group_by(Number) %>% filter(Value == 'Retail Support L3') %>% filter(Start == min(Start)) %>%
+  mutate(first_L3 = 1) %>% select(Number, Start, first_L3)
+
+# get first analyst responses for teams L1/L2/L3 ----
+all_history <- all_history %>% left_join(first_L1) %>% left_join(first_L2) %>% left_join(first_L3)
+
+all_history <- all_history %>% arrange(Number, Start) %>% group_by(Number) %>%
+  mutate(L1_response = case_when(Field == 'assigned_to' & prev_action == 'assignment_group' & lag(first_L1 == 1) ~ 1),
+         L2_response = case_when(Field == 'assigned_to' & prev_action == 'assignment_group' & lag(first_L2 == 1) ~ 1),
+         L3_response = case_when(Field == 'assigned_to' & prev_action == 'assignment_group' & lag(first_L3 == 1) ~ 1))
+
+first_L1_response <- all_history %>% filter(L1_response == 1) %>% 
+  select(Number, L1_Response_Analyst = Value, L1_Response_Time = response_time, L1_Time_of_Response = Start)
+first_L2_response <- all_history %>% filter(L2_response == 1) %>%
+  select(Number, L2_Response_Analyst = Value, L2_Response_Time = response_time, L2_Time_of_Response = Start)
+first_L3_response <- all_history %>% filter(L3_response == 1) %>%
+  select(Number, L3_Response_Analyst = Value, L3_Response_Time = response_time, L3_Time_of_Response = Start)
+
+# import incident list. join response data to incident list ----
+OnePOS_Incidents_Import <- readxl::read_excel(path = "\\\\cewp1650\\Chris Jabr Reports\\ONOW Exports\\incident.xlsx")
+out <- OnePOS_Incidents_Import %>% left_join(first_L1_response) %>% left_join(first_L2_response) %>% left_join(first_L3_response)
 
 
-## why are team history files & test different row counts???
+
+
+
+
+# notes:
+# all_history: most tickets sequentially start with team assignments (because of join order). just ~10 tickets do not.
+# Value == "" literally means analyst assignment was removed. shows "assigned to [Empty]" in ticket history
+
+# reference: convert string to POSIXct UTC time 
+# t1 <- ymd_hms("2018-07-19 08:08:22")
+
+# reference: workspace mgmt
+# rm(list=ls()[str_detect(ls(), "all_history")])
+
+# reference: benchmarking
+start_time <- Sys.time()
+writeLines(paste("Starting:", start_time))
+writeLines(paste0("Start time: ", start_time,
+                  "\nEnd time: ", Sys.time(),
+                  "\nElapsed time: ", round(difftime(Sys.time(),start_time, units='secs'),2), " seconds."))
